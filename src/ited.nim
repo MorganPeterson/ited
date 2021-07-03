728e889112d8740321d158c6b29149f7831d7a32
MorganPeterson <lastyearsmodel@gmail.com>
2021-07-03 18:52:10 -0400

    layout and close added

--- src/ited.nim
+++ src/ited.nim
@@ -10,15 +10,12 @@ when isMainModule:
   type
     View = ref object of RootObj
       commandValue: string
-    Commands = array[2, string]
+      editorText: string
+    Commands = array[3, string]
 
-  var editorText: string = ""
-  let AvailableCommands: Commands = ["open", "write"]
+  let AvailableCommands: Commands = ["open", "write", "close"]
 
-  proc readAFile(fn: string): string =
-    result = readFile(fn)
-
-  proc cmdRightClickHandler() =
+  proc cmdRightClickHandler(view: var View) =
     var cmd: string
     var arg: array[1, string]
 
@@ -32,19 +29,26 @@ when isMainModule:
           arg[0] = word
       if cmd == "open":
         try:
-          editorText = readAFile(arg[0])
+          view.editorText = readFile(arg[0])
+          view.commandValue = ""
         except IOError:
-          echo "IO Error"
+          view.commandValue = "IO Error"
       elif cmd == "write":
-        writeFile(arg[0], editorText)
+        try:
+          writeFile(arg[0], view.editorText)
+          view.commandValue = ""
+        except IOError:
+          view.commandValue = "IO Error"
+      elif cmd == "close":
+          view.commandValue = ""
+          view.editorText = ""
 
   proc renderView(view: var View) =
     frame "view":
       box 0, 0, parent.box.w, parent.box.h
-      itemSpacing 10
 
       rectangle "commandContainer":
-        box 1, 1, parent.box.w-2, 19
+        box 1, 0, parent.box.w-2, 19
         fill "#FFFFEA"
         strokeWeight 1
         stroke "#000000"
@@ -57,23 +61,26 @@ when isMainModule:
           multiline false
           binding view.commandValue
           onRightClick:
-            cmdRightClickHandler()
+            view.cmdRightClickHandler()
 
-      rectangle "workarea":
-        box 1, 19, parent.box.w-2, parent.box.h-20
-        strokeWeight 1
-        stroke "#000000"
-        fill "#FFFFEA"
+      frame "workarea":
+        box 1, 18, parent.box.w-2, parent.box.h-19
 
-        text "editor":
-          font "Inconsolata", 13.0, 400.0, 13, hLeft, vTop
-          box 2, 2, parent.box.w, parent.box.h
-          fill "#000000"
-          highlightColor "#AEEEEE"
-          multiline true
-          binding editorText
+        rectangle "editorContainer":
+          box 0, 0, parent.box.w, parent.box.h
+          strokeWeight 1
+          stroke "#000000"
+          fill "#FFFFEA"
+
+          text "editor":
+            font "Inconsolata", 13.0, 400.0, 13, hLeft, vTop
+            box 2, 2, parent.box.w, parent.box.h
+            fill "#000000"
+            highlightColor "#AEEEEE"
+            multiline true
+            binding view.editorText
   
-  var mainView = View(commandValue: "")
+  var mainView = View(commandValue: "", editorText: "")
 
   proc drawMain() =
     group "editor":
