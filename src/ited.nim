a60c0707cacbd9db2674c453821732b77db9463f
MorganPeterson <lastyearsmodel@gmail.com>
2021-07-03 18:10:12 -0400

    read and write

--- src/ited.nim
+++ src/ited.nim
@@ -1,3 +1,5 @@
+import strutils
+import sequtils
 import fidget
 import typography/textboxes
 
@@ -8,11 +10,33 @@ when isMainModule:
   type
     View = ref object of RootObj
       commandValue: string
+    Commands = array[2, string]
+
+  var editorText: string = ""
+  let AvailableCommands: Commands = ["open", "write"]
+
+  proc readAFile(fn: string): string =
+    result = readFile(fn)
 
   proc cmdRightClickHandler() =
-    let command = textBox.copy()
+    var cmd: string
+    var arg: array[1, string]
+
+    let command = textBox.cut()
+    
     if command.len > 0:
-      echo command
+      for word in split(command):
+        if AvailableCommands.anyIt(it == word):
+          cmd = word
+        else:
+          arg[0] = word
+      if cmd == "open":
+        try:
+          editorText = readAFile(arg[0])
+        except IOError:
+          echo "IO Error"
+      elif cmd == "write":
+        writeFile(arg[0], editorText)
 
   proc renderView(view: var View) =
     frame "view":
@@ -20,14 +44,14 @@ when isMainModule:
       itemSpacing 10
 
       rectangle "commandContainer":
-        box 1, 1, parent.box.w-2, 22
+        box 1, 1, parent.box.w-2, 19
         fill "#FFFFEA"
         strokeWeight 1
         stroke "#000000"
 
         text "command":
-          font "Inconsolata", 13.0, 400.0, 20, hLeft, vTop
-          box 2, 1, parent.box.w-2, 22
+          font "Inconsolata", 13.0, 400.0, 17, hLeft, vTop
+          box 2, 1, parent.box.w-2, 19
           fill "#000000"
           highlightColor "#AEEEEE"
           multiline false
@@ -36,10 +60,19 @@ when isMainModule:
             cmdRightClickHandler()
 
       rectangle "workarea":
-        box 1, 22, parent.box.w-2, parent.box.h-23
+        box 1, 19, parent.box.w-2, parent.box.h-20
         strokeWeight 1
         stroke "#000000"
+        fill "#FFFFEA"
 
+        text "editor":
+          font "Inconsolata", 13.0, 400.0, 13, hLeft, vTop
+          box 2, 2, parent.box.w, parent.box.h
+          fill "#000000"
+          highlightColor "#AEEEEE"
+          multiline true
+          binding editorText
+  
   var mainView = View(commandValue: "")
 
   proc drawMain() =
@@ -49,4 +82,7 @@ when isMainModule:
 
       renderView(mainView)
 
-startFidget(drawMain)
+  proc main() =
+    startFidget(drawMain)
+
+main()
