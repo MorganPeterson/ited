62c7b95e33bd02dbf880f5e3ddf839179217f92e
MorganPeterson <lastyearsmodel@gmail.com>
2021-07-03 16:45:45 -0400

    command selection

--- src/ited.nim
+++ src/ited.nim
@@ -1,29 +1,52 @@
 import fidget
+import typography/textboxes
 
 when isMainModule:
-
   loadFont("Inconsolata", "Inconsolata-Regular.ttf")
+  setTitle("It Ed. Hello.")
 
-  setTitle("Pad of Code")
+  type
+    View = ref object of RootObj
+      commandValue: string
 
-  var
-    textValue = """
-    -- sql query
-    SELECT foo
-    FROM bar
-    WHERE a = 234 and b = "nothing"
-    """
+  proc cmdRightClickHandler() =
+    let command = textBox.copy()
+    if command.len > 0:
+      echo command
+
+  proc renderView(view: var View) =
+    frame "view":
+      box 0, 0, parent.box.w, parent.box.h
+      itemSpacing 10
+
+      rectangle "commandContainer":
+        box 1, 1, parent.box.w-2, 22
+        fill "#FFFFEA"
+        strokeWeight 1
+        stroke "#000000"
+
+        text "command":
+          font "Inconsolata", 13.0, 400.0, 20, hLeft, vTop
+          box 2, 1, parent.box.w-2, 22
+          fill "#000000"
+          highlightColor "#AEEEEE"
+          multiline false
+          binding view.commandValue
+          onRightClick:
+            cmdRightClickHandler()
+
+      rectangle "workarea":
+        box 1, 22, parent.box.w-2, parent.box.h-23
+        strokeWeight 1
+        stroke "#000000"
+
+  var mainView = View(commandValue: "")
 
   proc drawMain() =
-    frame "main":
-      box 0, 0, parent.box.w-20, parent.box.h
-      font "Inconsolata", 16.0, 400.0, 20, hLeft, vTop
-      fill "#F7F7F9"
+    group "editor":
+      box 0, 0, parent.box.w, parent.box.h
+      fill "#FFFFFF"
 
-      text "codebox":
-        box 0, 0, parent.box.w, parent.box.h
-        fill "#000000"
-        multiline true
-        binding textValue
+      renderView(mainView)
 
 startFidget(drawMain)
