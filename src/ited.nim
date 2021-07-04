3a3a785feec29a35ce1652097b093005b65eead5
MorganPeterson <lastyearsmodel@gmail.com>
2021-07-04 11:59:51 -0400

    status update

--- src/ited.nim
+++ src/ited.nim
@@ -5,46 +5,71 @@ import fidget
 import typography/textboxes
 
 when isMainModule:
-  loadFont("Inconsolata", "Inconsolata-Regular.ttf")
+  loadFont("IBM Plex Sans", "IBMPlexSans-Regular.ttf")
   setTitle("It Ed. Hello.")
 
   type
     View = ref object of RootObj
+      dirty: int32
+      msg: string
+      fileName: string
       commandValue: string
       editorText: string
-    Commands = array[3, string]
+      statusValue: string
 
-  let AvailableCommands: Commands = ["open", "write", "close"]
+  const AvailableCommands: array[3, string] = ["o", "w", "c"]
 
   proc cmdRightClickHandler(view: var View) =
     var cmd: string
-    var arg: array[1, string]
+    var arg: string
 
-    let command = textBox.cut()
+    let command = textBox.copy().strip()
     
     if command.len > 0:
       for word in split(command):
         if AvailableCommands.anyIt(it == word):
           cmd = word
         else:
-          arg[0] = word
-      if cmd == "open":
-        try:
-          view.editorText = readFile(arg[0])
-          view.commandValue = ""
-        except IOError:
-          view.commandValue = "IO Error"
-      elif cmd == "write":
-        try:
-          writeFile(arg[0], view.editorText)
-          view.commandValue = ""
-        except IOError:
-          view.commandValue = "IO Error"
-      elif cmd == "close":
-          view.commandValue = ""
+          arg = word
+      case cmd
+        of AvailableCommands[0]:
+          # open - read file
+          try:
+            view.editorText = readFile(arg)
+            view.fileName = arg
+            view.dirty = 0
+            view.msg = "Open Success"
+          except IOError:
+            view.msg = "Open Failed"
+            discard
+        of AvailableCommands[1]:
+          # write - write to file
+          try:
+            writeFile(arg, view.editorText)
+            view.dirty = 0
+            view.msg = "Write Success"
+          except IOError:
+            view.msg = "Write Failed"
+            discard
+        of AvailableCommands[2]:
+          # close - remove text from buffer
           view.editorText = ""
-      else:
-          view.editorText = execProcess(command)
+          view.dirty = 0
+          view.fileName = ""
+        else:
+          # try to execute as os program
+          try:
+            view.editorText = execProcess(command)
+          except:
+            discard
+
+  proc computeStatusLine(view: var View): string =
+    let lines = view.editorText.countLines().intToStr() & "L | "
+    let words = view.editorText.count(' ').intToStr() & "W | "
+    let chars = view.editorText.len.intToStr() & "C "
+    let fn = "[ " & view.fileName & " ] "
+    let dy = "[ " & view.dirty.intToStr() & " ] "
+    result = dy & fn & lines & words & chars & " " & view.msg
 
   proc renderView(view: var View) =
     frame "view":
@@ -57,14 +82,23 @@ when isMainModule:
         stroke "#000000"
 
         text "command":
-          font "Inconsolata", 13.0, 400.0, 17, hLeft, vTop
-          box 2, 1, parent.box.w-2, 19
+          box 2, 2, parent.box.w-2, 19
           fill "#000000"
+          font "IBM Plex Sans", 14.0, 400.0, 15, hLeft, vTop
           highlightColor "#888ACA"
           multiline false
           binding view.commandValue
           onRightClick:
             view.cmdRightClickHandler()
+
+        text "status":
+          box parent.box.w / 2, 2, (parent.box.w / 2)-2, 19
+          fill "#000000"
+          font "IBM Plex Sans", 14.0, 400.0, 15, hLeft, vTop
+          multiline false
+          characters view.computeStatusLine()
+          textAutoResize tsWidthAndHeight
+          layoutAlign laStretch
 
       frame "workarea":
         box 1, 18, parent.box.w-2, parent.box.h-19
@@ -72,15 +106,18 @@ when isMainModule:
         rectangle "editorContainer":
           box 0, 0, parent.box.w, parent.box.h
           fill "#FFFFEA"
-
+          scrollBars true
+          
           text "editor":
-            font "Inconsolata", 13.0, 400.0, 13, hLeft, vTop
-            box 2, 2, parent.box.w, parent.box.h
+            box 2, 2, parent.box.w-2, parent.box.h-2
             fill "#000000"
+            font "IBM Plex Sans", 14.0, 400.0, 15, hLeft, vTop
             highlightColor "#888ACA"
             multiline true
             binding view.editorText
-  
+            onInput:
+              inc view.dirty
+        
   var mainView = View(commandValue: "", editorText: "")
 
   proc drawMain() =
