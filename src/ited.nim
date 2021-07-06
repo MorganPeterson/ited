f0232259b51f2ba9464adda5c7bbce5a70bf18fa
MorganPeterson <lastyearsmodel@gmail.com>
2021-07-06 18:50:25 -0400

    sweeps

--- src/ited.nim
+++ src/ited.nim
@@ -1,4 +1,6 @@
-## It Ed Hello Ed It Or
+## It Ed or Hello.
+## Write something for fun and profit.
+##
 
 import parsecfg
 import fidget
@@ -7,6 +9,7 @@ from os import expandTilde, fileExists
 from osproc import execProcess
 from typography/textboxes import typeCharacters, setCursor, adjustScroll, copy
 
+## default values to keep you safe
 const DefaultTitle = "It Ed. Hello."
 const DefaultTab = "    "
 const DefaultRegularFont = "/usr/share/fonts/TTF/DejaVuSans.ttf"
@@ -26,6 +29,7 @@ const DefaultConfigDirHome = expandTilde("~/.config/ited/ited.cfg")
 when isMainModule:
   type
     View = ref object of RootObj
+      ## global variable object
       dirty: bool
       msg: string
       cursorPos: int
@@ -37,6 +41,7 @@ when isMainModule:
       commandNode: Node
 
     Colors = ref object of RootObj
+      ## configurable colors for the app
       primary: string
       secondary: string
       highlight: string
@@ -44,21 +49,25 @@ when isMainModule:
       border: string
 
     Font = ref object of RootObj
+      ## font name and location in local file system
       name: string
       url: string
 
     Fonts = ref object of RootObj
+      ## All of our fonts and their size
       regular: Font
       italic: Font
       bold: Font
       size: float32
 
     Config = ref object of RootObj
+      ## holds our configuration file
       title: string
       tab: string
       fonts: Fonts
       colors: Colors
 
+  # initialize the main view
   var mainView = View(
     dirty: false,
     cursorPos: 0,
@@ -69,8 +78,10 @@ when isMainModule:
     statusValue: "")
 
   proc loadCfg(): Config =
+    ## load configuration file function
     var cfg: parsecfg.Config
 
+    # check if local file or system file exists and load it if it does
     if fileExists(DefaultConfigDirHome):
       cfg = loadConfig(DefaultConfigDirHome)
     elif fileExists(DefaultConfigDirEtc):
@@ -78,6 +89,7 @@ when isMainModule:
     else:
       cfg = newConfig()
 
+    # final configuration
     result = Config(
       title: getSectionValue(cfg, "Common", "Title", DefaultTitle),
       tab: getSectionValue(cfg, "Common", "Tab", DefaultTab),
@@ -105,15 +117,17 @@ when isMainModule:
       )
     )
 
+  # load the config if there is one
   let ItEdCfg = loadCfg()
 
+  # now that we have the configuration we can load the fonts and set the title
   loadFontAbsolute(ItEdCfg.fonts.regular.name, ItEdCfg.fonts.regular.url)
   loadFontAbsolute(ItEdCfg.fonts.italic.name, ItEdCfg.fonts.italic.url)
   loadFontAbsolute(ItEdCfg.fonts.bold.name, ItEdCfg.fonts.bold.url)
   setTitle(ItEdCfg.title)
 
   proc cmdHandler(view: var View, cmdType: string) =
-    ## try to execute as os program
+    ## handles all the commands sent from the user in the command line
     var cmd: string
 
     if cmdType == "Enter":
@@ -128,49 +142,46 @@ when isMainModule:
 
     try:
       let checkCmd = cmd.splitWhitespace(1)
-      # if we are writing out to a file
-      if checkCmd[0] == "w":
-        if checkCmd.len > 1:
-          var fn: string
+      case checkCmd[0]
+        of "w":
+          # open a file for writing
+          if checkCmd.len > 1:
+            var fn: string
+            for c in 1..(checkCmd.len-1):
+              fn = fn & checkCmd[c]
+            writefile(fn, view.editorText)
+          else:
+            writefile(view.fileName, view.editorText)
+          view.dirty = false
+          view.msg = "written"
+        of "o":
+          # open a file for reading
+          if checkCmd.len > 1:
+            var fn: string
+            for c in 1..(checkCmd.len-1):
+              fn = fn & checkCmd[c]
+            view.editorText = readfile(fn)
+            view.fileName = fn
+            view.cursorPos = 1
+          view.dirty = false
+          view.msg = ""
+        of "/", "?":
+          # search one way or the other but never both
+          var searchString: string
           for c in 1..(checkCmd.len-1):
-            fn = fn & checkCmd[c]
-          writefile(fn, view.editorText)
+            searchString = searchString & checkCmd[c]
+          if checkCmd[0] == "/":
+            view.cursorPos = view.editorText.find(searchString, start=Natural(view.cursorPos))
+          elif checkCmd[0] == "?":
+            view.cursorPos = view.editorText.rfind(searchString, last=Natural(view.cursorPos))
+          keyboard.focus(view.editorNode)
+          textBox.setCursor(view.cursorPos)
+          textBox.selector = view.cursorPos + searchString.len
+          textBox.adjustScroll()
         else:
-          writefile(view.fileName, view.editorText)
-        view.dirty = false
-        view.msg = "written"
-      elif checkCmd[0] == "o":
-        if checkCmd.len > 1:
-          var fn: string
-          for c in 1..(checkCmd.len-1):
-            fn = fn & checkCmd[c]
-          view.editorText = readfile(fn)
-          view.fileName = fn
+          # if not writing then go to command line
+          view.editorText = execProcess(cmd)
           view.cursorPos = 1
-        view.dirty = false
-        view.msg = ""
-      elif checkCmd[0] == "/":
-        var searchString: string
-        for c in 1..(checkCmd.len-1):
-          searchString = searchString & checkCmd[c]
-        view.cursorPos = view.editorText.find(searchString, start=Natural(view.cursorPos))
-        keyboard.focus(view.editorNode)
-        textBox.setCursor(view.cursorPos)
-        textBox.selector = view.cursorPos + searchString.len
-        textBox.adjustScroll()
-      elif checkCmd[0] == "?":
-        var searchString: string
-        for c in 1..(checkCmd.len-1):
-          searchString = searchString & checkCmd[c]
-        view.cursorPos = view.editorText.rfind(searchString, last=Natural(view.cursorPos))
-        keyboard.focus(view.editorNode)
-        textBox.setCursor(view.cursorPos)
-        textBox.selector = view.cursorPos + searchString.len
-        textBox.adjustScroll()
-      else:
-        # if not writing then go to command line
-        view.editorText = execProcess(cmd)
-        view.cursorPos = 1
     except:
       view.msg = getCurrentExceptionMsg()
       discard
@@ -183,7 +194,7 @@ when isMainModule:
       view.dirty = false
 
   proc computeStatusLine(view: var View): string =
-    ## create our string from the status
+    ## create our string for the status line
     var currentLine: string = "1"
     var currentChar: string = "1"
     var isDirty = ""
@@ -212,8 +223,10 @@ when isMainModule:
         multiline false
         binding view.commandValue
         onRightClick:
+          # click the command, padre!
           view.cmdHandler("RightClick")
         onInput:
+          # who is handling who?
           for buttonIdx in 0..<buttonDown.len:
             let button = Button(buttonIdx)
             if buttonDown[button]:
@@ -248,7 +261,7 @@ when isMainModule:
     frame "editor":
       box 1, 18, parent.box.w-2, parent.box.h-38
       fill ItEdCfg.colors.primary
-      clipContent true
+      clipContent true # get our scolling in order
 
       text "editorText":
         box 2, 2, parent.box.w-4, parent.box.h-2
@@ -258,6 +271,7 @@ when isMainModule:
         multiline true
         binding view.editorText
         onInput:
+          # looking for them custom buttons
           for buttonIdx in 0..<buttonDown.len:
             let button = Button(buttonIdx)
             if buttonDown[button]:
@@ -271,14 +285,12 @@ when isMainModule:
                 else:
                   discard
 
-              if button >= Button.SPACE and button <= Button.GRAVE_ACCENT:
-                view.dirty = true
-              elif button >= Button.ENTER and button <= Button.BACKSPACE:
-                view.dirty = true
-              elif button == Button.DELETE:
-                view.dirty = true
-              elif button >= Button.KP_0 and button < Button.KP_EQUAL:
-                view.dirty = true
+              # check if the user dirtied the text
+              case button
+                of Button.SPACE..Button.GRAVE_ACCENT, Button.ENTER..Button.BACKSPACE, Button.DELETE, Button.KP_0..Button.KP_EQUAL:
+                  view.dirty = true
+                else:
+                  discard
 
   proc renderView(view: var View) =
     ## render our editor view
